c41209b47d31265e6a70638fa76f592c06b10dd9
Axel Wegener <a.wegener@ifaoe.de>
2017-12-13 11:08:09 +0100

    fixed vim runtime paths

--- install_custom_vim.sh
+++ install_custom_vim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cat << EOF > ~/.vimrc
-set runtimepath+=~/.vim_runtime
-source ~/.vim_runtime/config.vim
+set runtimepath+=~/.vim_custom
+source ~/.vim_custom/config.vim
 EOF
 git clone --depth=1 --recursive https://github.com/awmath/vimrc.git ~/.vim_custom
 
